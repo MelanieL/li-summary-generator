933796e30f563090418537b3a7af3988f31ac7d5
MelanieL <melanieleeking@gmail.com>
2019-06-07 21:16:36 -0400

    Add javascript for button submit on form

---
+++ src/scripts/script.js
@@ -0,0 +1,7 @@
+const generateButton = document.querySelector('.generateButton');
+
+function saveAnswer(){
+    console.log('This works!');
+};
+
+generateButton.addEventListener('click', saveAnswer);
