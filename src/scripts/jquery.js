0c5a5da623f1269ac43802f403a724e9ba90909b
MelanieL <melanieleeking@gmail.com>
2020-02-13 21:48:06 -0500

    Fix prevent default bug on validation. Form validates fields and displays result.

--- src/scripts/jquery.js
+++ src/scripts/jquery.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    $('.generateButton').click(function () {
-        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-        return false;
-    });
+    // $('.generateButton').click(function () {
+    //     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    //     return false;
+    // });
     $('.resetButton').click(function () {
         $("html, body").animate({ scrollTop:0 }, "slow");
         return false;
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,14 @@
 const generateButton = document.querySelector('.generateButton');
 const resetButton = document.querySelector('.resetButton');
-const theForm = document.getElementById('form');
-const resultCntr = document.querySelector('.result')
+const form = document.getElementById('form');
+const resultCntr = document.querySelector('.result');
 const generatedSummary = document.querySelector('.summary');
 const answers = [];
 
 // This forces the window to open at the top. Implemented to avoid form reset confusion because page is refreshed.
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
-}
+};
 
 function showSummaryCntr() {
     resultCntr.classList.remove('hide');
@@ -22,7 +22,6 @@ function hideSummaryCntr() {
 
 function saveAnswer(event) {
     // Use a for each instead?
-    // event.preventDefault();
     console.log('Event has submitted');
     const firstName = document.getElementById('firstname').value;
     const title = document.getElementById('title').value;
@@ -33,22 +32,25 @@ function saveAnswer(event) {
     const softskill2 = document.getElementById('softskill2').value;
     const hobby = document.getElementById('hobby').value;
     const like = document.getElementById('like').value;
+    event.preventDefault();
     answers.push(firstName, title, industry, skill1, skill2, softskill1, softskill2, hobby, like);
     generateSummary();
 };
 
 function generateSummary(){
     const html =
-        `<p>My name is ${answers[0]} and I’m an experienced ${answers[1]} in the ${answers[2]} industry.</p>
-        <p>I specialize in ${answers[3]} and ${answers[4]}. My co-workers would say I’m ${answers[5]} and ${answers[6]}.</p>
-        <p>In my spare time, I enjoy ${answers[7]} and ${answers[8]}.</p>`
+    `<p>My name is ${answers[0]} and I’m an experienced ${answers[1]} in the ${answers[2]} industry.</p>
+    <p>I specialize in ${answers[3]} and ${answers[4]}. My co-workers would say I’m ${answers[5]} and ${answers[6]}.</p>
+    <p>In my spare time, I enjoy ${answers[7]} and ${answers[8]}.</p>`
     generatedSummary.innerHTML = html;
     showSummaryCntr();
+    window.scrollTo(0, document.body.scrollHeight);
 };
 
 function resetAll() {
     location.reload(true);
 };
 
-theForm.addEventListener('submit', saveAnswer);
+// generateButton.addEventListener('submit', saveAnswer);
+form.addEventListener('submit', saveAnswer);
 resetButton.addEventListener('click', resetAll);
